ddd9ae4d21b73db3e09e32c513c7022378eadd79
Monikarajpurohit1612 <monikarajpurohitm8@gmail.com>
2023-01-04 14:08:18 +0530

    g

---
+++ classwork/list slice.py
@@ -0,0 +1,22 @@
+#   0  1  2    3     4  5    6     7   8   9      0     1     2      3  4   +
+l =[1,12,222,"Tops",66,2.2,"True",1.1,88,"False",2002,66.6,"Python",55,50]
+#   5  4  3    2     1  0    9     8   7   6      5    4      3      2  1    -
+print(l)
+#for i in l:
+ #   print(i)
+#print("Tops"in l)
+
+print(l[:])
+print(l[:11])
+print(l[5:13])
+print(l[2:14:4])
+print(l[0:11:4])
+print(l[::])
+
+
+
+print(l[:])
+print(l[5:13])
+print(l[-5:-12])
+print(l[-1:-15:-1])
+print(l[-3:-11:-2])
---
+++ classwork/string.py
@@ -0,0 +1,17 @@
+  #012345678901234567 +
+s="tops technologies"
+  #876543210987654321 -
+  
+print (s)
+#positive values
+print(s[:10])   
+print(s[11:])
+print(s[2:16]) 
+print(s[1:17:2])
+print(s[::])
+print(s[:18:17]) 
+
+#nagative values
+print(s[-8:])
+print(s[-7:])
+print(s[-2:-16:-2])
